67cf608a9694108814b1a04a0c18ae25f684425f
Amirbahador Pv <amirbahador.pv@gmail.com>
2021-01-12 13:06:11 -0500

    use costume validator in brands model

--- accessitem/models.py
+++ accessitem/models.py
@@ -1,3 +1,14 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    title = models.CharField(max_length=512)
+
+    def clean(self):
+        if self.title.isdigit():
+            raise ValidationError("title must be meaningful not only digits")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
